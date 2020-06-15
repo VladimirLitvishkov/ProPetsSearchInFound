00d137d9669d94ffa3aee8c5b926d24026db947b
Vladimir <v_litva@mail.ru>
2020-06-15 11:57:01 +0300

    update 15.06.2020

--- ProPetsLFSearchInFound/src/main/java/propets/dao/searchinfound/FoundToSearchRepository.java
+++ ProPetsLFSearchInFound/src/main/java/propets/dao/searchinfound/FoundToSearchRepository.java
@@ -9,8 +9,8 @@ import propets.model.searchinfound.FoundToSearch;
 
 public interface FoundToSearchRepository extends ElasticsearchRepository<FoundToSearch, String> {
 
-	@Query("{\'bool\':{\'must\':[{\'match\':{\'type\':\'?0\'}},{\'match\':{\'breed\':\'?1\'}},{\'match\':{\'sex\':\'?2\'}},{\'match\':{\'tags\':{\'query\':\'?6\',\'minimum_should_match\':\'?7%\'}}}],\'filter\':{\'geo_distance\':{\'distance\':'?5km',\'coordinates\':{\'lat\':\'?3\',\'lon\':\'?4\'}}}}}")
-	Set<FoundToSearch> findPosts(String type, String breed, String sex, Double latitude, Double longitude, int radius,
+	@Query("{\'bool\':{\'must\':[{\'match\':{\'type\':\'?0\'}},{\'match\':{\'breed\':\'?1\'}},{\'match\':{\'sex\':\'?2\'}},{\'match\':{\'tags\':{\'query\':\'?6\',\'minimum_should_match\':\'?7%\'}}}],\'filter\':{\'geo_distance\':{\'distance\':\'?5km\',\'coordinates\':{\'lat\':\'?3\',\'lon\':\'?4\'}}}}}")
+	Set<FoundToSearch> findPosts(String type, String breed, String sex, Double lat, Double lon, int radius,
 			String tags, int minPercent);
 
 }
